52d7ab7471dda8c89d7c8e1d27109d9b487535b8
VoytenSvyat <voytensvyat@gmail.com>
2025-04-17 19:51:33 +0200

    added homework_03

--- lesson_03/demo20250417_2/src/Loader.java
+++ lesson_03/demo20250417_2/src/Loader.java
@@ -1,15 +1,20 @@
-public class Loader implements Runnable{
-    private String name;
-    private int nBox; // количество коробок
-    private int capacity; // сколько коробок за раз сможет перенести
-    private Warehouse warehouse;
-    private int done; // сколько коробок он уже перенес
+import java.util.Random;
 
-    public Loader(String name, int nBox, int capacity, Warehouse warehouse) {
+public class Loader implements Runnable{
+    private final String name;
+    private final int nBox;
+    private final int capacity;
+    private final Warehouse warehouse1;
+    private final Warehouse warehouse2;
+    private int done = 0; // Счётчик перенесённых коробок
+    private final Random random = new Random();
+
+    public Loader(String name, int nBox, int capacity, Warehouse warehouse1, Warehouse warehouse2) {
         this.name = name;
         this.nBox = nBox;
         this.capacity = capacity;
-        this.warehouse = warehouse;
+        this.warehouse1 = warehouse1;
+        this.warehouse2 = warehouse2;
     }
 
 
@@ -17,9 +22,13 @@ public class Loader implements Runnable{
     public void run() {
         while (done<nBox){
             int value = Math.min(nBox-done, capacity); // обьем переносящих коробок которие остались
-            warehouse.addValue(value);
+            warehouse1.addValue(value / 2);
+            warehouse2.addValue(value / 2);
             done+=capacity;
         }
         System.out.println(name + " finish. Get: " + done + " boxes" );
+        if (Main.winnerFound.compareAndSet(false, true)) {
+            System.out.println(name +  " финиширует первим и получает бонус!!");
+        }
     }
 }
--- lesson_03/demo20250417_2/src/Main.java
+++ lesson_03/demo20250417_2/src/Main.java
@@ -1,17 +1,22 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Main {
     private static final int N_BOX = 1000;
-    private static final int CAPACITY = 1;
+    private static final int CAPACITY = 2;
+    static final AtomicBoolean winnerFound = new AtomicBoolean(false); // Флаг победителя
 
-    public static void main(String[] args) {
-        Warehouse warehouse = new Warehouse("#1");
+    public static void main(String[] args) throws InterruptedException {
+        Warehouse warehouse1 = new Warehouse("#1");
+        Warehouse warehouse2 = new Warehouse("#2");
         Thread[] loaders = {
-                new Thread(new Loader("Jack", N_BOX, CAPACITY, warehouse)),
-                new Thread(new Loader("John", N_BOX, CAPACITY, warehouse)),
-                new Thread(new Loader("Nick", N_BOX, CAPACITY, warehouse)),
+                new Thread(new Loader("Jack", N_BOX, CAPACITY, warehouse1, warehouse2)),
+                new Thread(new Loader("John", N_BOX, CAPACITY, warehouse1, warehouse2)),
+                new Thread(new Loader("Nick", N_BOX, CAPACITY, warehouse1, warehouse2)),
         };
 
         for (Thread thread : loaders){
             thread.start();
+            thread.sleep(15);
         }
 
         for(Thread thread : loaders){
@@ -22,6 +27,7 @@ public class Main {
             }
         }
 
-        System.out.println(warehouse);
+        System.out.println(warehouse1);
+        System.out.println(warehouse1);
     }
 }
