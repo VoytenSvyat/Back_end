b1ef7abc1c37b0fad925e1bbadab1230f8878782
VoytenSvyat <voytensvyat@gmail.com>
2025-05-01 09:15:59 +0200

    upgrade homework_10

--- lesson_10/demo_3/src/main/java/de/ait/Main.java
+++ lesson_10/demo_3/src/main/java/de/ait/Main.java
@@ -11,6 +11,13 @@ import java.net.URI;
 
 
 public class Main {
+    /*
+    # 1
+Ваша задача написать используя API https://api.imagga.com эндпоинт /text написать программу которая распознает текст на картинке. Для тестирования можно использовать
+картинку https://cdn.javarush.com/images/article/431abcb1-71aa-4137-97bd-ad26d7aa0e00/800.jpeg
+Результат для этой картинки строка: "ОТВЕЧАЮ В СПРИНГЕ ЭТО УЖЕ ЕСТЬ"
+
+     */
     public static void main(String[] args) {
         String imgUrl = "https://cdn.javarush.com/images/article/431abcb1-71aa-4137-97bd-ad26d7aa0e00/800.jpeg";
         String lang = "de,en,ru";
